224f7d046b5df191ab051767a9bde1136aea52a2
rafaelsous <rafaelsousa.souza@gmail.com>
2020-12-13 18:50:01 -0300

    Config LOCALE_ID para pt-BR.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -41,6 +41,10 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
   providers: [
     RestaurantsService,
     ShoppingCartService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    }
   ],
   bootstrap: [AppComponent]
 })
