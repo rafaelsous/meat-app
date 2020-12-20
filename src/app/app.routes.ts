cffcbafcf69ae95fe2d24f5324865247b052fe97
rafaelsous <rafaelsousa.souza@gmail.com>
2020-12-20 20:34:30 -0300

    Add order component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,12 @@ import { AboutComponent } from "./about/about.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { OrderComponent } from "./order/order.component";
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'order', component: OrderComponent },
   { path: 'restaurants', component: RestaurantsComponent },
   { path: 'restaurants/:id', component: RestaurantDetailComponent,
     children: [
