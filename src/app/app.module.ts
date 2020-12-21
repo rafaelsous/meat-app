8621f63a6947df2a35094b09741b7b3e045b1a7c
rafaelsous <rafaelsousa.souza@gmail.com>
2020-12-20 21:01:38 -0300

    Add template forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
 
@@ -38,6 +39,7 @@ import { OrderComponent } from './order/order.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
